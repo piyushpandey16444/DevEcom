23de82d391f4037bb646ff960603c58d4cb4c54b
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-06-15 22:44:00 +0530

    Created Models

--- store/models.py
+++ store/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Category(models.Model):
@@ -16,5 +19,24 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey("store.Category", on_delete=models.CASCADE, related_name='product')
-    
+    category = models.ForeignKey(
+        "store.Category", on_delete=models.CASCADE, related_name='product')
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='product_creator')
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250, default='admin')
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-created_on')
+
+    def __str__(self):
+        return self.title
