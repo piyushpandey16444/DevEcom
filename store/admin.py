1515c112bc56e18e2dca968cfa7b5f8a073898ff
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-06-15 23:09:38 +0530

    added items for Product and Category

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from store.models import Product, Category
 
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
 
 
-
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price',
+                    'in_stock', 'created_on', 'updated_on']
+    list_filter = ['in_stock', 'is_active']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('title',)}
