c6fbfedbf530c7f0e8cb9de34557f532f4679654
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-06-15 23:52:36 +0530

    models and views testing done.

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from store.models import Product, Category
+from django.contrib.auth.models import User
 
 
 class TestCategory(TestCase):
@@ -16,3 +17,27 @@ class TestCategory(TestCase):
         """
         data = self.data1
         self.assertTrue(isinstance(data, Category))
+
+    def test_category_model_name(self):
+        """
+        Test Category model default name
+        """
+        data = self.data1
+        self.assertEqual(str(data), 'django')
+
+
+class TestProduct(TestCase):
+
+    def setUp(self):
+        category = Category.objects.create(name='django', slug='django')
+        user = User.objects.create(username='admin')
+        self.data1 = Product.objects.create(
+            category_id=category.id, title='django basics', created_by_id=user.id, slug='django-basics', price='20.00', image='django')
+
+    def test_product_model_entry(self):
+        """
+        Test Product model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), 'django basics')
