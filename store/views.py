4f4fd3c8d87c5c07b506223c7c11f46b30608de1
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-06-16 00:09:01 +0530

    adding templates

--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import Product, Category
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def all_products(request):
+    products = get_list_or_404(Product)
+    return render(request, 'store/home.html', {'products': products})
