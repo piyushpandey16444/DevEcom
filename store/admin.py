4dd6ffeece504a18911ae25ec8f0fc88615748f3
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-06-15 22:59:57 +0530

    models registered

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from store.models import Product, Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+
+
