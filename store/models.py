df0074e018350b8b997b0d0b2c58352ddd0c682b
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-06-15 22:37:01 +0530

    models creation

--- store/models.py
+++ store/models.py
@@ -4,4 +4,17 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse("store:category_list", args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey("store.Category", on_delete=models.CASCADE, related_name='product')
     
