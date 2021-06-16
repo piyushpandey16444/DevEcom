7dfbc36661186388b4244051c3995dc591bff908
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-06-17 00:12:12 +0530

    adding dynamic links

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -38,3 +39,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("store:product_detail", args=[self.slug])
