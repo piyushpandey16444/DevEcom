67bcb4e7b29b09131d0afa466eef96b24343591b
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-06-16 23:46:15 +0530

    Adding product detail urls

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Product, Category
 from django.contrib.auth.models import User
 
@@ -6,3 +6,8 @@ from django.contrib.auth.models import User
 def all_products(request):
     products = get_list_or_404(Product)
     return render(request, 'store/home.html', {'products': products})
+
+
+def product_detail(request, slug):
+    product_obj = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store/product_detail.html', {'product': product_obj})
